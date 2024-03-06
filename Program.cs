445db6b53c49d29d78f5bdbe149b3537daed2866
Tyler-Breithaupt <breith61@mail.nmc.edu>
2024-03-06 17:09:27 -0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+public class Animal<T>
+{
+    // Generic property
+    public T data;
+
+    // Constructor
+    public Animal(T data)
+    {
+        this.data = data;
+        Console.WriteLine("Data passed: " + this.data);
+    }
+
+    public T getAnimal()
+    {
+        return data;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Animal<string> animalName = new Animal<string>("Lion");
+        Animal<string> animalHabitat = new Animal<string>("Savannah");
+        Animal<bool> endangered = new Animal<bool>(false);
+        Animal<bool> extinct = new Animal<bool>(false);
+        Animal<int> averageLifespan = new Animal<int>(15);
+    }
+}
